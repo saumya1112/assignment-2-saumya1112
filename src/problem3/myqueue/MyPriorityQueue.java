215dac0a88b1e3ae34840f7856e4316bd682a515
saumya gupta <saumya.gupta_cs18@gla.ac.in>
2020-03-28 17:29:39 +0530

    create private sortedAdd method

--- src/problem3/myqueue/MyPriorityQueue.java
+++ src/problem3/myqueue/MyPriorityQueue.java
@@ -28,6 +28,21 @@ public class MyPriorityQueue<E> implements adt<E> {
         return response;
     }
 
+    private void sortedAdd(E data) {
+        Node<E> node = new Node<>(data);
+        for (int i = 0; i < size; i++) {
+            Node<E> node1 = getNode(size - i);
+            if (node.getData().equals(node1.getData())) {
+                node.setNext(node1.getNext());
+                node1.setNext(node);
+            } else
+                continue;
+        }
+        rear = getNode(size);
+        front = getNode(1);
+        size++;
+    }
+
     @Override
     public void add(E data) {
 
