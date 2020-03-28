a1e555639d417a16a8cf227104e6bd867a20b5aa
saumya gupta <saumya.gupta_cs18@gla.ac.in>
2020-03-28 18:26:46 +0530

    create setter and getter of all fields

--- src/problem5/student/Student.java
+++ src/problem5/student/Student.java
@@ -7,4 +7,31 @@
 package problem5.student;
 // to store student information and properties
 public class Student {
+    private String name;
+    private int rollNo;
+    private int backlog;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getRollNo() {
+        return rollNo;
+    }
+
+    public void setRollNo(int rollNo) {
+        this.rollNo = rollNo;
+    }
+
+    public int getBacklog() {
+        return backlog;
+    }
+
+    public void setBacklog(int backlog) {
+        this.backlog = backlog;
+    }
 }
