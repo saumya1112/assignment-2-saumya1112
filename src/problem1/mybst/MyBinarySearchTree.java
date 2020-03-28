2ec8cacdb11bb680a818ccfdef5e8617640daadd
saumya gupta <saumya.gupta_cs18@gla.ac.in>
2020-03-28 14:13:15 +0530

    implements fields and add method

--- src/problem1/mybst/MyBinarySearchTree.java
+++ src/problem1/mybst/MyBinarySearchTree.java
@@ -5,7 +5,17 @@
  *  Time: 7:17 PM
  */
 package problem1.mybst;
-// to implement BinarySearchTree
-public class MyBinarySearchTree {
 
+import problem1.node.TreeNode;
+
+// to implement BinarySearchTree
+public class MyBinarySearchTree<E> implements BinarySearchTreeadt<E> {
+        public int numberOfLeftChild = 0;
+        private TreeNode<E> root;
+        private int maxLevel = 0;
+
+        @Override
+        public boolean add(E data) {
+                return false;
+        }
 }
