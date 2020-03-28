be3175875497609dd595959079c85ec76dfdc3cc
saumya gupta <saumya.gupta_cs18@gla.ac.in>
2020-03-28 17:26:04 +0530

    create methods and fields

--- src/problem3/myqueue/MyPriorityQueue.java
+++ src/problem3/myqueue/MyPriorityQueue.java
@@ -6,5 +6,27 @@
  */
 package problem3.myqueue;
 
-public class MyPriorityQueue {
+
+import problem3.adt.adt;
+import problem3.node.Node;
+
+public class MyPriorityQueue<E> implements adt<E> {
+    private int size = 0;
+    private Node<E> front;
+    private Node<E> rear;
+
+    @Override
+    public void add(E data) {
+
+    }
+
+    @Override
+    public E remove() {
+        return null;
+    }
+
+    @Override
+    public E peek() {
+        return null;
+    }
 }
