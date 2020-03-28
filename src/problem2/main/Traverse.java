1927dbda9a3da181b9f5567bac2cfebade8f3578
saumya gupta <saumya.gupta_cs18@gla.ac.in>
2020-03-28 16:52:21 +0530

    create insert method

--- src/problem2/main/Traverse.java
+++ src/problem2/main/Traverse.java
@@ -7,4 +7,28 @@ public class Traverse {
         return root;
     }
 
+    public void insert(int data) {
+        TreeNode node = new TreeNode(data);
+        if (root == null) {
+            root = node;
+        } else {
+            TreeNode temp = root;
+            TreeNode parent = null;
+            while (temp != null) {
+                parent = temp;
+                if (data <= temp.getData()) {
+                    temp = temp.getLeft();
+                } else {
+                    temp = temp.getRight();
+                }
+            }
+            if (node.getData() <= parent.getData()) {
+                parent.setLeft(node);
+            } else {
+                parent.setRight(node);
+            }
+        }
+    }
+
+
 }
