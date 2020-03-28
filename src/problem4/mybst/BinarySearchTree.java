399072aed4af1f4866c9c7aecb4be7757e06aade
saumya gupta <saumya.gupta_cs18@gla.ac.in>
2020-03-28 18:06:38 +0530

    create TreeNode insert() method

---
+++ src/problem4/mybst/BinarySearchTree.java
@@ -0,0 +1,19 @@
+package problem4.mybst;
+
+import problem1.node.TreeNode;
+
+import java.util.Queue;
+
+public class BinarySearchTree {
+    static Queue<TreeNode> q;
+
+    public TreeNode insert(TreeNode root, int x) {
+        if (root == null)
+            return new TreeNode(x);
+        else if (x > (int) root.getData())
+            root.setRight(insert(root.getRight(), x));
+        else
+            root.setLeft(insert(root.getLeft(), x));
+        return root;
+    }
+}
