5b61fa2496a543feb518d2480f1896341ef58c18
saumya gupta <saumya.gupta_cs18@gla.ac.in>
2020-03-28 17:15:48 +0530

    create getter and setter of rollno and name

---
+++ src/problem3/student/student.java
@@ -0,0 +1,31 @@
+package problem3.student;
+
+public class student {
+    private int rollNumber;
+    private String name;
+
+    public student(int rollNumber, String name) {
+        this.rollNumber = rollNumber;
+        this.name = name;
+    }
+
+    public student(int rollNumber) {
+        this.rollNumber = rollNumber;
+    }
+
+    public int getRollNumber() {
+        return rollNumber;
+    }
+
+    public void setRollNumber(int rollNumber) {
+        this.rollNumber = rollNumber;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
