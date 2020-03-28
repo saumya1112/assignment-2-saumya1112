e32ac66d2a1fdf50812d0e2a09e22b896a7640da
saumya gupta <saumya.gupta_cs18@gla.ac.in>
2020-03-28 17:50:14 +0530

    construct getter ,setter,consuctor of all field

--- src/problem4/myqueue/Node.java
+++ src/problem4/myqueue/Node.java
@@ -1,4 +1,33 @@
 package problem4.myqueue;
 
 public class Node {
+    private TreeNode node;
+    private Node next;
+
+    public Node(int data) {
+        node = new TreeNode(data);
+        next = null;
+    }
+
+    public Node(TreeNode treeNode) {
+        node = treeNode;
+        next = null;
+    }
+
+    public TreeNode getNode() {
+        return node;
+    }
+
+    public void setNode(TreeNode node) {
+        this.node = node;
+    }
+
+    public Node getNext() {
+        return next;
+    }
+
+    public void setNext(Node next) {
+        this.next = next;
+    }
+
 }
