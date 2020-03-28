6c47d0eef04c449d9f16f52c0106776615c46854
saumya gupta <saumya.gupta_cs18@gla.ac.in>
2020-03-28 14:04:02 +0530

    make constructer and getter and setter of all fields

--- src/problem1/node/TreeNode.java
+++ src/problem1/node/TreeNode.java
@@ -13,6 +13,29 @@ public class TreeNode<E> {
 
     public TreeNode(E data) {
         this.data = data;
+    }
 
+    public E getData() {
+        return data;
+    }
+
+    public void setData(E data) {
+        this.data = data;
+    }
+
+    public TreeNode<E> getLeft() {
+        return left;
+    }
+
+    public void setLeft(TreeNode<E> left) {
+        this.left = left;
+    }
+
+    public TreeNode<E> getRight() {
+        return right;
+    }
+
+    public void setRight(TreeNode<E> right) {
+        this.right = right;
     }
 }
