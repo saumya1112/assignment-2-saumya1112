2e15589d20d17f3e65f5d27b91ff2f1c266b3947
saumya gupta <saumya.gupta_cs18@gla.ac.in>
2020-03-28 14:31:50 +0530

    give input and output

--- src/problem1/main/MyMain.java
+++ src/problem1/main/MyMain.java
@@ -12,7 +12,16 @@ package problem1.main;
 import problem1.mybst.MyBinarySearchTree;
 
 public class MyMain {
-    MyBinarySearchTree<Integer> myBinarySearchTree = new MyBinarySearchTree<>();
+    public static void main(String[] args) {
+        MyBinarySearchTree<Integer> myBinarySearchTree = new MyBinarySearchTree<>();
+        myBinarySearchTree.add(15);
+        myBinarySearchTree.add(10);
+        myBinarySearchTree.add(50);
+        myBinarySearchTree.add(55);
+        myBinarySearchTree.add(60);
+        myBinarySearchTree.print();
+        myBinarySearchTree.print();
+        System.out.println("Number of nodes who does not have a left Child : " + myBinarySearchTree.numberOfLeftChild);
 
-
+    }
 }
