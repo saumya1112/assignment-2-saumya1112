6afc1c9a612f0229240e702d778a5520ec137fdf
saumya gupta <saumya.gupta_cs18@gla.ac.in>
2020-03-28 17:53:43 +0530

    create getter and setter and MyQueue method

--- src/problem4/myqueue/MyQueue.java
+++ src/problem4/myqueue/MyQueue.java
@@ -10,4 +10,51 @@ package problem4.myqueue;
 public class MyQueue {
     private Node front, end, temp;
     private int size;
+
+    public MyQueue() {
+        front = null;
+        end = null;
+        temp = null;
+        size = 0;
+    }
+
+    public Node getFront() {
+        return front;
+    }
+
+    public void setFront(Node front) {
+        this.front = front;
+    }
+
+    public Node getEnd() {
+        return end;
+    }
+
+    public void setEnd(Node end) {
+        this.end = end;
+    }
+
+    public Node getTemp() {
+        return temp;
+    }
+
+    public void setTemp(Node temp) {
+        this.temp = temp;
+    }
+
+    public int getSize(MyQueue queue) {
+        queue.temp = queue.front;
+        while (queue.temp != null) {
+            ++size;
+            queue.temp = queue.temp.getNext();
+        }
+        queue.temp = queue.front;
+        return size++;
+    }
+
+    public void setSize(int size) {
+        this.size = size;
+    }
+
+
 }
