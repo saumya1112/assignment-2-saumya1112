eaa0da98cab8866b22e933f8d8f759573cf5486b
saumya gupta <saumya.gupta_cs18@gla.ac.in>
2020-03-28 16:58:02 +0530

    create input

--- src/problem2/main/MyMain.java
+++ src/problem2/main/MyMain.java
@@ -5,7 +5,21 @@
  *  Time: 7:15 PM
  */
 package problem2.main;
+
 // executable class
 // use problem1.mybst.MyBinarySearchTree class for creating binary tree
 public class MyMain {
+    public static void main(String[] args) {
+        Traverse prePostOrder = new Traverse();
+        prePostOrder.insert(100);
+        prePostOrder.insert(200);
+        prePostOrder.insert(300);
+        prePostOrder.insert(400);
+        prePostOrder.insert(500);
+        prePostOrder.insert(600);
+        prePostOrder.insert(700);
+        prePostOrder.insert(800);
+        System.out.println("PreOrder traverse :");
+    }
 }
+
